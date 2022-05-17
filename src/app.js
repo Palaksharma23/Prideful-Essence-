f666f7c42d64fa073f9d0b6ac49f64b9259ec27f
palaksharma23 <mahimasharma2312@gmail.com>
2022-05-17 18:00:30 +0530

    Added Backend Implementation Files

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path"); 
+const hbs = require("hbs");
+const async = require("hbs/lib/async");
+const res = require("express/lib/response");
+
+require('dotenv').config()
+ 
+const app = express();
+const port = process.env.PORT || 3000;
+
+const staticpath = path.join(__dirname,"../public");
+const templates = path.join(__dirname,"../templates/views");
+const partials = path.join(__dirname,"../templates/partials");
+
+
+app.use(express.urlencoded({extended:false})) 
+
+app.use(express.static(staticpath));
+app.set("views",templates);
+hbs.registerPartials(partials);
+
+app.set("view engine","hbs");
+app.get("/",(req,res)=>{
+    res.render("index");
+}) 
+
+app.get("/signup",(req,res)=>{
+    res.render("signup");
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+
+app.get("/chatroom",(req,res)=>{
+    res.render("chatroom");
+})
+
+app.get("/contact",(req,res)=>{
+    res.render("contact");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running at port no ${port}`);
+})
